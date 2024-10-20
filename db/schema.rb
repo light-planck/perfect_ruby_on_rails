893c0c3c7bef0fbfb6d7b805e0719a7c262e874b
light-planck <seseifunc16@gmail.com>
2024-10-20 20:42:03 +0900

    feat: db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_10_19_150309) do
+ActiveRecord::Schema[7.2].define(version: 2024_10_20_114055) do
+  create_table "books", force: :cascade do |t|
+    t.string "name"
+    t.date "published_on"
+    t.integer "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "tasks", force: :cascade do |t|
     t.text "content"
     t.datetime "created_at", null: false
