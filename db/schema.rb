6bf98701a2cd570cd7b726cdbd116f6e839847fd
light-planck <seseifunc16@gmail.com>
2024-10-20 22:45:05 +0900

    feat: add publisher id to books

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_10_20_114055) do
+ActiveRecord::Schema[7.2].define(version: 2024_10_20_134226) do
+  create_table "authors", force: :cascade do |t|
+    t.string "name"
+    t.string "penname"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "books", force: :cascade do |t|
     t.string "name"
     t.date "published_on"
     t.integer "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "publishers", force: :cascade do |t|
+    t.string "name"
+    t.text "address"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
